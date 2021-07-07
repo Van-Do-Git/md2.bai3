ca4e1977cee37f8fa20c0882b050b6026a426a5d
unknown <mittervan@gmail.com>
2021-07-07 13:29:51 +0700

    find Min in array, used method

--- src/Main5.java
+++ src/Main5.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main5 {
     public static void main(String[] args) {
-
+        int[] arr1 = getArr();
+        System.out.printf("\nValue min in array is: \t%d\t at index\t%d", min(arr1)[0], min(arr1)[1]);
     }
+
+    static int[] getArr() {
+        int size;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input size your array");
+        size = input.nextInt();
+        int setarr[] = new int[size];
+        System.out.println("Element in araay:");
+        for (int i = 0; i < size; i++) {
+            setarr[i] = (int) Math.round(Math.random() * 100);
+            System.out.print(setarr[i] + "\t");
+        }
+        return setarr;
+    }
+
+    static int[] min(int arr[]) {
+        int result[] = new int[2];
+        int min = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        result[0] = min;
+        result[1] = index;
+        return result;
+    }
+
 }
